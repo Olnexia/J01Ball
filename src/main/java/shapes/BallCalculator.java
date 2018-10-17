a6e4ed1d76e421bfba3a6cc7137ff655870cb162
Andrey Losik <olnexia@gmail.com>
2018-10-18 00:29:12 +0300

    Added Ball class implementation and some methods to BallCalculator class

--- src/main/java/shapes/BallCalculator.java
+++ src/main/java/shapes/BallCalculator.java
@@ -1,4 +1,28 @@
 package shapes;
 
 public class BallCalculator {
+
+    public double getSurfaceArea(Ball ball){
+        int radius = ball.getRadius();
+        return 4*Math.PI*radius*radius;
+    }
+
+    public double getVolume(Ball ball){
+        int radius = ball.getRadius();
+        return (4.0/3.0)*Math.PI*radius*radius*radius;
+    }
+
+    public double getVolumeRatio(Ball ball,CoordPlane plane){
+        //code here
+        return 0.0;
+    }
+
+    public boolean isBall(Ball ball){
+        return ball.getRadius()>0;
+    }
+
+    public boolean isTouchedByPlane(Ball ball){
+        //code here
+        return false;
+    }
 }
